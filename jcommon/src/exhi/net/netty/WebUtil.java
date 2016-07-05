2d3d9b96e69eb84a5722c319cbbfad15fd78cdf9
Rain <xzzhuo@icloud.com>
2016-07-05 21:21:20 +0800

    Add "freemarker" jar library support

---
+++ jcommon/src/exhi/net/netty/WebUtil.java
@@ -0,0 +1,58 @@
+package exhi.net.netty;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import exhi.net.interface1.NetCharset;
+import exhi.net.utils.NetUtils;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+public class WebUtil {
+
+	private NetProcess mProcess = null;
+	private Map<String, Object> mParam = new HashMap<String, Object>();
+	private String mTemplatePath = ".";
+	
+	public WebUtil(NetProcess process)
+	{
+		mProcess = process;
+		mTemplatePath = System.getProperty("user.dir");
+	}
+	
+	public void setTemplatePath(String path)
+	{
+		mTemplatePath = path;
+	}
+	
+	public void assign(String key, Object value)
+	{
+		mParam.put(key, value);
+	}
+	
+	public void display(String name)
+	{
+		NetCharset charset = NetHttpHelper.instance().getConfig().getCharset();
+		try {
+			@SuppressWarnings("deprecation")
+			Configuration cfg = new Configuration();
+			cfg.setDirectoryForTemplateLoading(new File(mTemplatePath));
+			Template template = cfg.getTemplate(name, NetUtils.AdapterContentCharset(charset));
+			
+			StringWriter sw = new StringWriter();
+			template.process(mParam, sw);
+			sw.flush();
+			mProcess.setResponseText(new StringBuilder(sw.getBuffer()));
+		} catch (TemplateException e) {
+			mProcess.setResponseText(new StringBuilder(e.getMessage()));
+			e.printStackTrace();
+		} catch (IOException e) {
+			mProcess.setResponseText(new StringBuilder(e.getMessage()));
+			e.printStackTrace();
+		}
+	}
+}
